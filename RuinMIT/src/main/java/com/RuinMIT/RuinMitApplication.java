752617dd581247eb8ba41731c105cd8b71a34683
meet702 <meetchhabhaiya10@gmail.com>
2026-05-19 21:48:18 +0530

    initial commit

---
+++ RuinMIT/src/main/java/com/RuinMIT/RuinMitApplication.java
@@ -0,0 +1,13 @@
+package com.RuinMIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuinMitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuinMitApplication.class, args);
+	}
+
+}
---
+++ RuinMIT/src/test/java/com/RuinMIT/RuinMitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RuinMIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuinMitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
