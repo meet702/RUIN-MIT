1efe1fdef99a7e1cc611bd174c5695067c290605
meet702 <meetchhabhaiya10@gmail.com>
2026-07-07 22:07:27 +0530

    fixed backend websocket url error

--- backend/src/main/java/com/RuinMIT/config/WebSocketConfig.java
+++ backend/src/main/java/com/RuinMIT/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.RuinMIT.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -15,6 +16,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final WebSocketJwtInterceptor webSocketJwtInterceptor;
 
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic", "/queue");
@@ -24,11 +28,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins(
-                        "http://localhost:3000",
-                        "http://127.0.0.1:3000",
-                        "http://localhost:5173",
-                        "http://127.0.0.1:5173")
+                .setAllowedOrigins(frontendUrl.split(","))
                 .withSockJS();
     }
 
