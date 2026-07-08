3eafa5634e606564391466b31df4282404f894bb
meet702 <meetchhabhaiya10@gmail.com>
2026-07-08 23:08:16 +0530

    Added a debug statement

--- backend/src/main/java/com/RuinMIT/config/CloudinaryConfig.java
+++ backend/src/main/java/com/RuinMIT/config/CloudinaryConfig.java
@@ -21,6 +21,7 @@ public class CloudinaryConfig {
 
     @Bean
     public Cloudinary cloudinary() {
+        System.out.println("Cloud name: [" + cloudName + "], key length: " + (apiKey != null ? apiKey.length() : "null"));
         return new Cloudinary(Map.of(
                 "cloud_name", cloudName,
                 "api_key", apiKey,
